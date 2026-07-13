3b95a65a72edf03f86ddcacb86072052d07161bb
Ulysses260 <2486632227@qq.com>
2026-07-13 19:31:53 +0800

    Update report_generator.py

--- tactical_lens/report_generator.py
+++ tactical_lens/report_generator.py
@@ -34,7 +34,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_RIGHT, TA_JUSTIFY
 from reportlab.platypus import (
     BaseDocTemplate, PageTemplate, Frame, Paragraph, Spacer,
-    Table, TableStyle, Image, PageBreak, KeepTogether,
+    Table, TableStyle, Image, PageBreak, KeepTogether, SimpleDocTemplate,
 )
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
